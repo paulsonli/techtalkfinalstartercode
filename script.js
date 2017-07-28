39247ea889a29cc7761c1e52ec789e548ca30566
Paulson Li <paulsonli99@gmail.com>
2017-07-28 15:56:54 +0000

    website

--- script.js
+++ script.js
@@ -2,5 +2,27 @@
 // JavaScript File
 $( document ).ready(function() {
 
+$("#Submit").click(function(){
+    var submitTimeInfo = $("#time").val();
+    var submitDayInfo = $("#day").val();
+    var sumbitEmailInfo = $("#email").val();
+    if (submitTimeInfo != "" && subitDayInfo != "" && sumbitEmailInfo!= "") {
+        
+    }
+    
+    
+    
+});
+
+
+
+
+
+
+
+
+
+
+
 
 });
